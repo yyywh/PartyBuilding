0ad228e320c0592cc2d3bcdc73285af5b6bccdd0
ct <1486935673@qq.com>
2018-04-19 11:29:23 +0800

    新增测试类

---
+++ app/src/main/java/cn/ywh/partybuilding/activity/TestActivity.java
@@ -0,0 +1,17 @@
+package cn.ywh.partybuilding.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Ywh on 2018/4/19.
+ * Email：787875249@qq.com
+ * Function：
+ * Company：四川复兴科技有限公司
+ */
+
+public class TestActivity extends BaseAcivity {
+    @Override
+    public void initView(Bundle savedInstanceState) {
+
+    }
+}
